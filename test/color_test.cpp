971524e8357099b99787e649813d7743263053ab
Daisuke Deguchi <ddeguchi@nagoya-u.jp>
2003-11-22 14:26:18 +0000

    color用のテストファイルの追加忘れ

---
+++ test/color_test.cpp
@@ -0,0 +1,31 @@
+#include <mist/config/color.h>
+#include <iostream>
+
+int main( int argc, char *argv[] )
+{
+	using namespace std;
+
+	mist::rgb< signed int > v1( 1, 2, 3 ), v2( 1, 2, 4 );
+
+	cout << " ( A, B ) = ( " << v1 << ", " << v2 << " )" << endl;
+	cout << " 1. Operator A + B = \t" << ( v1 + v2 ) << endl;
+	cout << " 2. Operator A - B = \t" << ( v1 - v2 ) << endl;
+	cout << " 3. Operator A * B = \t" << ( v1 * v2 ) << endl;
+	cout << " 4. Operator A / B = \t" << ( v1 / v2 ) << endl;
+
+	cout << " 5. Operator A % B = \t" << ( v1 % v2 ) << endl;
+	cout << " 6. Operator A | B = \t" << ( v1 | v2 ) << endl;
+	cout << " 7. Operator A & B = \t" << ( v1 & v2 ) << endl;
+	cout << " 8. Operator A ^ B = \t" << ( v1 ^ v2 ) << endl;
+
+	cout << " 9. Operator A == B = \t" << ( v1 == v2 ) << endl;
+	cout << "10. Operator A != B = \t" << ( v1 != v2 ) << endl;
+	cout << "11. Operator A <  B = \t" << ( v1 <  v2 ) << endl;
+	cout << "12. Operator A <= B = \t" << ( v1 <= v2 ) << endl;
+	cout << "13. Operator A >  B = \t" << ( v1 >  v2 ) << endl;
+	cout << "14. Operator A >= B = \t" << ( v1 >= v2 ) << endl;
+
+	//cout << "15. Size of bool = \t" << sizeof( value< bool > ) << endl;
+	//cout << "16. Size of int  = \t" << sizeof( value< int > ) << endl;
+	//cout << "17. Size of char = \t" << sizeof( value< char > ) << endl;
+}
