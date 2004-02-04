a424f6879c37f040f7d7f8b30e6440ef81c63a63
Daisuke Deguchi <ddeguchi@nagoya-u.jp>
2004-02-04 04:45:46 +0000

    ラベリングの余分なコードを削除し，2次元と3次元でソースを共有． 警告を適切に処理． 行列演算用のLAPACKラッパの実装を開始．

--- labeling.h
+++ labeling.h
@@ -18,6 +18,41 @@ _MIST_BEGIN
 
 namespace __labeling_controller__
 {
+	template < class T > struct default_label_num2
+	{
+		_MIST_CONST( size_t, value, 2560 );
+	};
+	template < > struct default_label_num2< char >
+	{
+		_MIST_CONST( size_t, value, 127 );
+	};
+	template < > struct default_label_num2< signed char >
+	{
+		_MIST_CONST( size_t, value, 127 );
+	};
+	template < > struct default_label_num2< unsigned char >
+	{
+		_MIST_CONST( size_t, value, 255 );
+	};
+
+	template < class T > struct default_label_num3
+	{
+		_MIST_CONST( size_t, value, 10000 );
+	};
+	template < > struct default_label_num3< char >
+	{
+		_MIST_CONST( size_t, value, 127 );
+	};
+	template < > struct default_label_num3< signed char >
+	{
+		_MIST_CONST( size_t, value, 127 );
+	};
+	template < > struct default_label_num3< unsigned char >
+	{
+		_MIST_CONST( size_t, value, 255 );
+	};
+
+
 	template < int nc >
 	struct neighbor3
 	{
@@ -70,7 +105,8 @@ namespace __labeling_controller__
 
 		template < class T, class Allocator >
 		static inline void neighbor( array2< T, Allocator > &in, typename array2< T, Allocator >::value_type *l,
-								typename array2< T, Allocator >::size_type i, typename array2< T, Allocator >::size_type j, typename array2< T, Allocator >::size_type w )
+								typename array2< T, Allocator >::size_type i, typename array2< T, Allocator >::size_type j, typename array2< T, Allocator >::size_type k,
+								typename array2< T, Allocator >::size_type w, typename array2< T, Allocator >::size_type h )
 		{
 			l[ 0 ] = ( i > 0 && j > 0     ) ? in( i - 1, j - 1 ) : 0;
 			l[ 1 ] = ( j > 0              ) ? in( i    , j - 1 ) : 0;
@@ -87,7 +123,8 @@ namespace __labeling_controller__
 
 		template < class T, class Allocator >
 		static inline void neighbor( array2< T, Allocator > &in, typename array2< T, Allocator >::value_type *l,
-								typename array2< T, Allocator >::size_type i, typename array2< T, Allocator >::size_type j, typename array2< T, Allocator >::size_type w )
+								typename array2< T, Allocator >::size_type i, typename array2< T, Allocator >::size_type j, typename array2< T, Allocator >::size_type k,
+								typename array2< T, Allocator >::size_type w, typename array2< T, Allocator >::size_type h )
 		{
 			l[ 0 ] = ( j > 0 ) ? in( i    , j - 1 ) : 0;
 			l[ 1 ] = ( i > 0 ) ? in( i - 1, j     ) : 0;
@@ -95,18 +132,18 @@ namespace __labeling_controller__
 		}
 	};
 
-	template < class T, class Allocator, class neighbor >
-	typename array3< T, Allocator >::size_type labeling( array3< T, Allocator > &in, typename array3< T, Allocator >::value_type max_label, const neighbor dmy )
+	template < class Array, class neighbor >
+	typename Array::size_type labeling( Array &in, typename Array::size_type max_label, const neighbor dmy )
 	{
-		typedef typename array3< T, Allocator >::size_type  size_type;
-		typedef typename array3< T, Allocator >::value_type value_type;
+		typedef typename Array::size_type  size_type;
+		typedef typename Array::value_type value_type;
 
 		size_type i, j, k, ii, jj, kk, pos;
 		size_type array_num = neighbor::array_num;
 		size_type w = in.width( );
 		size_type h = in.height( );
 		size_type d = in.depth( );
-		value_type label = 0;
+		size_type label = 0;
 		value_type l[ neighbor::array_num ];
 		value_type min, max, nmin;
 		value_type m, n;
@@ -154,10 +191,10 @@ namespace __labeling_controller__
 								label++;
 
 								// テーブルに新しいラベルを登録
-								t[ label ] = label;
+								t[ label ] = static_cast< value_type >( label );
 
 								// 画像にラベルをつける
-								in( i, j, k ) = label;
+								in( i, j, k ) = static_cast< value_type >( label );
 							}
 							else
 							{
@@ -167,7 +204,7 @@ namespace __labeling_controller__
 
 								for( ii = 1 ; ii <= label ; ii++ )
 								{
-									t[ ii ] = t[ ii ] < ii ? t[ t[ ii ] ] : t[ ii ];
+									t[ ii ] = t[ ii ] < static_cast< value_type >( ii ) ? t[ t[ ii ] ] : t[ ii ];
 								}
 
 								for( ii = 1 ; ii <= label ; ii++ )
@@ -198,7 +235,7 @@ namespace __labeling_controller__
 								}
 
 								// テーブルの中を元に戻す
-								for( ii = 1 ; ii <= n ; ii++ )
+								for( ii = 1 ; ii <= static_cast< size_type >( n ) ; ii++ )
 								{
 									t[ ii ] = static_cast< value_type >( ii );
 								}
@@ -213,8 +250,8 @@ namespace __labeling_controller__
 									return ( max_label + 1 );
 								}
 
-								t[ label ] = label;
-								in( i, j, k ) = label;
+								t[ label ] = static_cast< value_type >( label );
+								in( i, j, k ) = static_cast< value_type >( label );
 							}
 						}
 						else
@@ -247,7 +284,7 @@ namespace __labeling_controller__
 							{
 								for( ii = 1 ; ii <= label ; ii++ )
 								{
-									t[ ii ] = t[ ii ] < ii ? t[ t[ ii ] ] : t[ ii ];
+									t[ ii ] = t[ ii ] < static_cast< value_type >( ii ) ? t[ t[ ii ] ] : t[ ii ];
 								}
 
 								nmin = t[ buf[ 0 ] ];
@@ -287,221 +324,7 @@ namespace __labeling_controller__
 
 		for( ii = 1 ; ii <= label ; ii++ )
 		{
-			t[ ii ] = t[ ii ] < ii ? t[ t[ ii ] ] : t[ ii ];
-		}
-
-		for( ii = 1 ; ii <= label ; ii++ )
-		{
-			if( t[ ii ] > n )
-			{
-				m = t[ ii ];
-
-				for( jj = 1 ; jj <= label ; jj++ )
-				{
-					t[ jj ] = t[ jj ] == m ? n : t[ jj ];
-				}
-			}
-
-			n = t[ ii ] >= n ? n + 1 : n;
-		}
-
-		label = n - 1;
-
-		for( i = 0 ; i < in.size( ) ; i++ )
-		{
-			in[ i ] = t[ in[ i ] ];
-		}
-
-		delete [] t;
-		return ( label );
-	}
-
-	template < class T, class Allocator, class neighbor >
-	typename array2< T, Allocator >::size_type labeling( array2< T, Allocator > &in, typename array2< T, Allocator >::value_type max_label, const neighbor labeling_type )
-	{
-		typedef typename array2< T, Allocator >::size_type  size_type;
-		typedef typename array2< T, Allocator >::value_type value_type;
-
-		size_type i, j, ii, jj, pos;
-		size_type array_num = neighbor::array_num;
-		size_type w = in.width( );
-		size_type h = in.height( );
-		value_type label = 0;
-		value_type l[ neighbor::array_num ];
-		value_type min, max, nmin;
-		value_type m, n;
-		value_type buf[ neighbor::array_num - 1 ];
-		bool flag;
-		value_type *t = new value_type[ max_label + 1 ];
-
-		for( i = 0 ; i < max_label ; i++ )
-		{
-			t[ i ] = 0;
-		}
-
-		for( j = 0 ; j < h ; j++ )
-		{
-			for( i = 0 ; i < w ; i++ )
-			{
-				if( in( i, j ) != 0 )
-				{
-					// 近傍の半分の取り出し
-					neighbor::neighbor( in, l, i, j, w );
-
-					// 近傍値の最大・最小を求める
-					min = max = l[ 0 ];
-
-					for( ii = 1 ; ii < array_num - 1 ; ii++ )
-					{
-						if( min > l[ ii ] )
-						{
-							min = l[ ii ];
-						}
-						else if( max < l[ ii ] )
-						{
-							max = l[ ii ];
-						}
-					}
-
-					if( min == 0 && max == 0 )
-					{
-						// 近傍値がすべて0なら
-						if( label < max_label )
-						{
-							// 次のラベルがテーブルの大きさ以下なら
-							label++;
-
-							// テーブルに新しいラベルを登録
-							t[ label ] = label;
-
-							// 画像にラベルをつける
-							in( i, j ) = label;
-						}
-						else
-						{
-							// もしラベル番号がいっぱいならガベージコレクション
-							// テーブルの中の整理
-							n = 1;
-
-							for( ii = 1 ; ii <= label ; ii++ )
-							{
-								t[ ii ] = t[ ii ] < ii ? t[ t[ ii ] ] : t[ ii ];
-							}
-
-							for( ii = 1 ; ii <= label ; ii++ )
-							{
-								if( t[ ii ] > n )
-								{
-									m = t[ ii ];
-
-									for( jj = 1 ; jj <= label ; jj++ )
-									{
-										t[ jj ] = t[ jj ] == m ? n : t[ jj ];
-									}
-								}
-
-								n = t[ ii ] >= n ? n + 1 : n;
-							}
-
-							// ラベル番号の付け直し
-							for( jj = 0 ; jj <= j ; jj++ )
-							{
-								for( ii = 0 ; ii <= i ; ii++ )
-								{
-									in( ii, jj ) = t[ in( ii, jj ) ];
-								}
-							}
-
-							// テーブルの中を元に戻す
-							for( ii = 1 ; ii <= n ; ii++ )
-							{
-								t[ ii ] = static_cast< value_type >( ii );
-							}
-
-							label = n;
-
-							// それでも足りない
-							if( label >= max_label )
-							{
-								std::cerr << " TABLE is NOT ENOUGH" << std::endl;
-								delete [] t;
-								return ( max_label + 1 );
-							}
-
-							t[ label ] = label;
-							in( i, j ) = label;
-						}
-					}
-					else
-					{
-						pos = 0;
-
-						// 近傍値の中の数値をとってくる
-						for( ii = 0 ; ii < array_num - 1 ; ii++ )
-						{
-							flag = false;
-
-							for( jj = 0 ; jj < pos ; jj++ )
-							{
-								if( buf[ jj ] == l[ ii ] && l[ ii ] > 0 )
-								{
-									flag = true;
-									break;
-								}
-							}
-
-							if( !flag && l[ ii ] > 0 )
-							{
-								buf[ pos ] = l[ ii ];
-								pos++;
-							}
-						}
-
-						// 近傍値の中で最も小さい値をつける
-						if( pos > 1 )
-						{
-							for( ii = 1 ; ii <= label ; ii++ )
-							{
-								t[ ii ] = t[ ii ] < ii ? t[ t[ ii ] ] : t[ ii ];
-							}
-
-							nmin = t[ buf[ 0 ] ];
-
-							for( ii = 0 ; ii < pos ; ii++ )
-							{
-								nmin = t[ buf[ ii ] ] < nmin ? t[ buf[ ii ] ] : nmin;
-							}
-
-							for( ii = 0 ; ii < pos ; ii++ )
-							{
-								if( t[ buf[ ii ] ] > nmin )
-								{
-									for( jj = 1 ; jj <= label ; jj++ )
-									{
-										t[ jj ] = t[ jj ] == t[ buf[ ii ] ] ? nmin : t[ jj ];
-									}
-								}
-
-								t[ buf[ ii ] ] = nmin;
-							}
-
-							in( i, j ) = nmin;
-						}
-						else
-						{
-							in( i, j ) = t[ buf[ 0 ] ];
-						}
-					}
-				}
-			}
-		}
-
-		// テーブルの整理
-		n = 1;
-
-		for( ii = 1 ; ii <= label ; ii++ )
-		{
-			t[ ii ] = t[ ii ] < ii ? t[ t[ ii ] ] : t[ ii ];
+			t[ ii ] = t[ ii ] < static_cast< value_type >( ii ) ? t[ t[ ii ] ] : t[ ii ];
 		}
 
 		for( ii = 1 ; ii <= label ; ii++ )
@@ -533,7 +356,7 @@ namespace __labeling_controller__
 
 
 template < class T1, class T2, class Allocator1, class Allocator2 >
-typename array2< T2, Allocator2 >::size_type labeling4( const array2< T1, Allocator1 > &in, array2< T2, Allocator2 > &out, typename array2< T2, Allocator2 >::value_type max_label = 0 )
+typename array2< T2, Allocator2 >::size_type labeling4( const array2< T1, Allocator1 > &in, array2< T2, Allocator2 > &out, typename array2< T2, Allocator2 >::size_type max_label = __labeling_controller__::default_label_num2< T2 >::value )
 {
 	typedef typename array2< T2, Allocator2 >::size_type  size_type;
 	typedef typename array2< T2, Allocator2 >::value_type value_type;
@@ -555,7 +378,7 @@ typename array2< T2, Allocator2 >::size_type labeling4( const array2< T1, Alloca
 }
 
 template < class T1, class T2, class Allocator1, class Allocator2 >
-typename array2< T2, Allocator2 >::size_type labeling8( const array2< T1, Allocator1 > &in, array2< T2, Allocator2 > &out, typename array2< T2, Allocator2 >::value_type max_label = 0 )
+typename array2< T2, Allocator2 >::size_type labeling8( const array2< T1, Allocator1 > &in, array2< T2, Allocator2 > &out, typename array2< T2, Allocator2 >::size_type max_label = __labeling_controller__::default_label_num2< T2 >::value )
 {
 	typedef typename array2< T2, Allocator2 >::size_type  size_type;
 	typedef typename array2< T2, Allocator2 >::value_type value_type;
@@ -577,7 +400,7 @@ typename array2< T2, Allocator2 >::size_type labeling8( const array2< T1, Alloca
 }
 
 template < class T1, class T2, class Allocator1, class Allocator2 >
-typename array3< T2, Allocator2 >::size_type labeling6( const array3< T1, Allocator1 > &in, array3< T2, Allocator2 > &out, typename array3< T2, Allocator2 >::value_type max_label = 0 )
+typename array3< T2, Allocator2 >::size_type labeling6( const array3< T1, Allocator1 > &in, array3< T2, Allocator2 > &out, typename array3< T2, Allocator2 >::size_type max_label = __labeling_controller__::default_label_num3< T2 >::value )
 {
 	typedef typename array3< T2, Allocator2 >::size_type  size_type;
 	typedef typename array3< T2, Allocator2 >::value_type value_type;
@@ -601,7 +424,7 @@ typename array3< T2, Allocator2 >::size_type labeling6( const array3< T1, Alloca
 
 
 template < class T1, class T2, class Allocator1, class Allocator2 >
-typename array3< T2, Allocator2 >::size_type labeling26( const array3< T1, Allocator1 > &in, array3< T2, Allocator2 > &out, typename array3< T2, Allocator2 >::value_type max_label = 0 )
+typename array3< T2, Allocator2 >::size_type labeling26( const array3< T1, Allocator1 > &in, array3< T2, Allocator2 > &out, typename array3< T2, Allocator2 >::size_type max_label = __labeling_controller__::default_label_num3< T2 >::value )
 {
 	typedef typename array3< T2, Allocator2 >::size_type  size_type;
 	typedef typename array3< T2, Allocator2 >::value_type value_type;
