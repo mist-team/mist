0e53144a32d7070bdcb176375317a180da73610d
Daisuke Deguchi <ddeguchi@nagoya-u.jp>
2004-04-12 10:20:58 +0000

    多倍長整数の初期化を文字列で行えるようにした． あと，各種演算子の実装完了．

--- mist/config/integer.h
+++ mist/config/integer.h
@@ -15,34 +15,20 @@ _MIST_BEGIN
 
 // 以下のコードは Tomy 氏が http://www5.airnet.ne.jp/tomy/cpro/csource.htm にて
 // 掲載しているソースを基に作成し，演算ミス等のバグを修正し，独自の拡張を施したものである．
+// 基底は256であり，無駄なビットが発生しないようにしてある
 
 
-namespace __integer_util__
-{
-	template < unsigned int N >
-	struct log10
-	{
-		_MIST_CONST( unsigned int, value, log10< N / 10 >::value + 1 );
-	};
-
-	template < >
-	struct log10< 1 >
-	{
-		_MIST_CONST( unsigned int, value, 0 );
-	};
-}
-
-
-template < unsigned int MAX_KETA, class T = int, unsigned int BASE = 10000 >
+template < unsigned int MAX_KETA >
 struct integer
 {
 public:
 	typedef size_t size_type;
 	typedef ptrdiff_t difference_type;
-	typedef T value_type;
+	typedef unsigned char value_type;
 
 private:
-	_MIST_CONST( unsigned int, DATA_NUM, MAX_KETA / __integer_util__::log10< BASE >::value + 1 );
+	_MIST_CONST( unsigned int, BASE, 256 );
+	_MIST_CONST( unsigned int, DATA_NUM, 2 * MAX_KETA / 5 + 2 );
 
 protected:
 	size_t		length_;
@@ -77,6 +63,11 @@ public:
 	integer( const integer &v ) : length_( v.length_ ), sign_( v.sign_ )
 	{
 		memcpy( data_, v.data_, sizeof( value_type ) * DATA_NUM );
+	}
+
+	integer( const ::std::string &str ) : length_( 0 ), sign_( true )
+	{
+		operator =( read( str ) );
 	}
 
 	const integer &operator =( const integer &a )
@@ -148,15 +139,11 @@ public:
 
 	const integer &operator *=( const integer &a )
 	{
-		if( length_ == 0 )
-		{
-			return( *this );
-		}
-		else if( a.length_ == 0 )
+		if( length_ * a.length_ == 0 )
 		{
 			return( operator =( integer( 0 ) ) );
 		}
-		if( length_ == 1 )
+		else if( length_ == 1 )
 		{
 			value_type t = sign_ ? data_[ 0 ] : -data_[ 0 ];
 			return( operator =( integer( a ) *= t ) );
@@ -183,7 +170,7 @@ public:
 		x.sign_ = sign_ == a.sign_;
 
 		size_type i, j, imax;
-		value_type u, t;
+		difference_type u, t;
 
 		for( j = 0 ; j < a.length_ ; j++ )
 		{
@@ -195,25 +182,22 @@ public:
 				for( i = j ; i <= imax ; i++ )
 				{
 					t += ( x.data_[ i ] + u * data_[ i - j ] );
-					x.data_[ i ] = t % BASE;
+					x.data_[ i ] = static_cast< value_type >( t % BASE );
 					t /= BASE;
 				}
 				t += x.data_[ i ];
 				while( t >= BASE )
 				{
-					x.data_[ i++ ] = t - BASE;
+					x.data_[ i++ ] = static_cast< value_type >( t - BASE );
 					t = x.data_[ i ] + 1;
 				}
-				x.data_[ i ] = t;
+				x.data_[ i ] = static_cast< value_type >( t );
 			}
 		}
-		if( x.data_[ length_ + a.length_ - 2 ] == 0 )
+		x.length_ = length_ + a.length_ - 1;
+		if( x.data_[ x.length_ - 1 ] == 0 )
 		{
-			x.length_ = length_ + a.length_ - 2;
-		}
-		else
-		{
-			x.length_ = length_ + a.length_ - 1;
+			x.length_--;
 		}
 		return( operator =( x ) );
 	}
@@ -299,7 +283,7 @@ public:
 
 		integer q, w;
 		integer aa( *this ), bb( a );
-		value_type d = BASE / ( a.data_[ a.length_ - 1 ] + 1 ), x;
+		difference_type d = BASE / ( a.data_[ a.length_ - 1 ] + 1 ), x;
 		q.sign_ = sign_ == a.sign_;
 
 		if( d != 1 )
@@ -329,7 +313,7 @@ public:
 				}
 				x--;
 			}
-			q.data_[ ql-- - 1 ] = x;
+			q.data_[ ql-- - 1 ] = static_cast< value_type >( x );
 			aa -= w;
 			if( aa.length_ == 0 || ql == 0 )
 			{
@@ -367,7 +351,7 @@ public:
 			n = - n;
 		}
 
-		value_type t = 0;
+		difference_type t = 0;
 		for( difference_type i = length_ - 1 ; i >= 0 ; i-- )
 		{
 			t = t * BASE + data_[ i ];
@@ -386,9 +370,82 @@ public:
 		return( operator =( *this - ( *this / a ) ) );
 	}
 
-	const integer &operator %=( value_type n )
+	const integer &operator %=( difference_type n )
 	{
 		return( operator =( *this - ( *this / n ) ) );
+	}
+
+
+	const integer &operator &=( const integer &a )
+	{
+		for( size_type i = 0 ; i < DATA_NUM ; i++ )
+		{
+			data_[ i ] &= a.data_[ i ];
+		}
+		length_ = DATA_NUM;
+		for( difference_type j = DATA_NUM - 1 ; j >= 0 ; j-- )
+		{
+			if( data_[ length_ - 1 ] != 0 )
+			{
+				break;
+			}
+			length_--;
+		}
+		return( *this );
+	}
+
+	const integer &operator |=( const integer &a )
+	{
+		for( size_type i = 0 ; i < DATA_NUM ; i++ )
+		{
+			data_[ i ] |= a.data_[ i ];
+		}
+		length_ = length_ > a.length_ ? length_ : a.length_;
+		return( *this );
+	}
+
+	const integer &operator ^=( const integer &a )
+	{
+		for( size_type i = 0 ; i < DATA_NUM ; i++ )
+		{
+			data_[ i ] ^= a.data_[ i ];
+		}
+		length_ = DATA_NUM;
+		for( difference_type j = DATA_NUM - 1 ; j >= 0 ; j-- )
+		{
+			if( data_[ length_ - 1 ] != 0 )
+			{
+				break;
+			}
+			length_--;
+		}
+		return( *this );
+	}
+
+	integer &operator ++( ) // 前置型
+	{
+		*this += 1;
+		return( *this );
+	}
+
+	const integer operator ++( int ) // 後置型
+	{
+		integer old_val( *this );
+		*this += 1;
+		return( old_val );
+	}
+
+	integer &operator --( ) // 前置型
+	{
+		*this -= 1;
+		return( *this );
+	}
+
+	const integer operator --( int ) // 後置型
+	{
+		integer old_val( *this );
+		*this -= 1;
+		return( old_val );
 	}
 
 
@@ -419,21 +476,69 @@ public:
 		{
 			return( "0" );
 		}
-
-		::std::string s = sign_ ? "" : "-";
-		char tmp[ 20 ];
-		static char format[ 20 ];
-		sprintf( tmp, "%d", data_[ length_ - 1 ] % BASE );
-		s += tmp;
-
-		sprintf( format, "%%0%dd", __integer_util__::log10< BASE >::value );
-
-		for( difference_type i = length_ - 2 ; i >= 0 ; i-- )
+		else if( length_ == 1 )
 		{
-			sprintf( tmp, format, data_[ i ] );
-			s += tmp;
+			char tmp[ 20 ];
+			sprintf( tmp, "%d", data_[ 0 ] );
+			return( ( sign_ ? "" : "-" ) + ::std::string( tmp ) );
 		}
-		return( s );
+		else
+		{
+			::std::string s = "";
+			integer dmy( *this );
+			while( dmy.length_ != 0 )
+			{
+				difference_type t = 0;
+				for( difference_type i = dmy.length_ - 1 ; i >= 0 ; i-- )
+				{
+					t = t * BASE + dmy.data_[ i ];
+					dmy.data_[ i ] = static_cast< value_type >( t / 10 );
+					t %= 10;
+				}
+				if( dmy.data_[ dmy.length_ - 1 ] == 0 )
+				{
+					dmy.length_--;
+				}
+				s = static_cast< char >( '0' + t ) + s;
+			}
+			return( ( sign_ ? "" : "-" ) + s );
+		}
+	}
+
+	int to_int32( ) const
+	{
+		if( length_ == 0 )
+		{
+			return( 0 );
+		}
+		else if( length_ > 4 )
+		{
+			// オーバーフロー
+			std::cerr << "overflow!!" << std::endl;
+			return( 0 );
+		}
+		else
+		{
+			return( ( sign_ ? 1 : -1 ) * ( data_[ 0 ] + BASE * ( data_[ 1 ] + BASE * ( data_[ 2 ] + BASE * data_[ 3 ] ) ) ) );
+		}
+	}
+
+	int to_uint32( ) const
+	{
+		if( length_ == 0 )
+		{
+			return( 0 );
+		}
+		else if( length_ > 4 )
+		{
+			// オーバーフロー
+			std::cerr << "overflow!!" << std::endl;
+			return( 0 );
+		}
+		else
+		{
+			return( data_[ 0 ] + BASE * ( data_[ 1 ] + BASE * ( data_[ 2 ] + BASE * data_[ 3 ] ) ) );
+		}
 	}
 
 protected:
@@ -478,7 +583,7 @@ protected:
 		size_type i;
 		integer x( 0 );
 
-		value_type t = 0;
+		difference_type t = 0;
 
 		for( i = 0 ; i < b.length_ ; i++ )
 		{
@@ -486,12 +591,12 @@ protected:
 
 			if( t < BASE )
 			{
-				x.data_[ i ] = t;
+				x.data_[ i ] = static_cast< value_type >( t );
 				t = 0;
 			}
 			else
 			{
-				x.data_[ i ] = t - BASE;
+				x.data_[ i ] = static_cast< value_type >( t - BASE );
 				t = 1;
 			}
 		}
@@ -502,12 +607,12 @@ protected:
 
 			if( t < BASE )
 			{
-				x.data_[ i ] = t;
+				x.data_[ i ] = static_cast< value_type >( t );
 				t = 0;
 			}
 			else
 			{
-				x.data_[ i ] = t - BASE;
+				x.data_[ i ] = static_cast< value_type >( t - BASE );
 				t = 1;
 			}
 
@@ -554,7 +659,7 @@ protected:
 		size_type i;
 		integer x( 0 );
 
-		value_type t = 0;
+		difference_type t = 0;
 
 		for( i = 0 ; i < b.length_ ; i++ )
 		{
@@ -562,12 +667,12 @@ protected:
 
 			if( t >= 0 )
 			{
-				x.data_[ i ] = t;
+				x.data_[ i ] = static_cast< value_type >( t );
 				t = 0;
 			}
 			else
 			{
-				x.data_[ i ] = t + BASE;
+				x.data_[ i ] = static_cast< value_type >( t + BASE );
 				t = 1;
 			}
 		}
@@ -578,12 +683,12 @@ protected:
 
 			if( t >= 0 )
 			{
-				x.data_[ i ] = t;
+				x.data_[ i ] = static_cast< value_type >( t );
 				t = 0;
 			}
 			else
 			{
-				x.data_[ i ] = t + BASE;
+				x.data_[ i ] = static_cast< value_type >( t + BASE );
 				t = 1;
 			}
 
@@ -658,31 +763,91 @@ protected:
 		}
 	}
 
+	static integer read( const ::std::string &str )
+	{
+		if( str.size( ) == 0 )
+		{
+			return( integer( 0 ) );
+		}
+		else
+		{
+			size_type i, k = 0;
+			bool sign = true;
+			if( str[ 0 ] == '-' )
+			{
+				sign = false;
+				k++;
+			}
+			else if( str[ 0 ] == '+' )
+			{
+				k++;
+			}
+
+			if( str.size( ) - k < 10 )
+			{
+				return( integer( ::atoi( str.c_str( ) ) ) );
+			}
+
+			::std::string s = "";
+			for( i = k ; i < str.size( ) ; i++ )
+			{
+				// 符号を除いた部分をコピーする．
+				if( '0' <= str[ i ] && str[ i ] <= '9' )
+				{
+					s += str[ i ];
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			integer x( 0 );
+			integer base( 100000000 ), b( 1 );
+			while( !s.empty( ) )
+			{
+				difference_type len = s.size( );
+				if( len > 8 )
+				{
+					x += integer( ::atoi( ( s.substr( len - 8 ) ).c_str( ) ) ) * b;
+					b *= base;
+					s = s.substr( 0, len - 8 );
+				}
+				else
+				{
+					x += integer( ::atoi( s.c_str( ) ) ) * b;
+					s = "";
+				}
+			}
+			x.sign_ = sign;
+			return( x );
+		}
+	}
 };
 
-template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator +( const integer< MAX_KETA, T, BASE > &v1, const integer< MAX_KETA, T, BASE > &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) += v2 ); }
-template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator -( const integer< MAX_KETA, T, BASE > &v1, const integer< MAX_KETA, T, BASE > &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) -= v2 ); }
-template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator *( const integer< MAX_KETA, T, BASE > &v1, const integer< MAX_KETA, T, BASE > &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) *= v2 ); }
-template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator /( const integer< MAX_KETA, T, BASE > &v1, const integer< MAX_KETA, T, BASE > &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) /= v2 ); }
-template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator %( const integer< MAX_KETA, T, BASE > &v1, const integer< MAX_KETA, T, BASE > &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) %= v2 ); }
-//template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator |( const integer< MAX_KETA, T, BASE > &v1, const integer< MAX_KETA, T, BASE > &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) |= v2 ); }
-//template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator &( const integer< MAX_KETA, T, BASE > &v1, const integer< MAX_KETA, T, BASE > &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) &= v2 ); }
-//template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator ^( const integer< MAX_KETA, T, BASE > &v1, const integer< MAX_KETA, T, BASE > &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) ^= v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator +( const integer< MAX_KETA > &v1, const integer< MAX_KETA > &v2 ){ return( integer< MAX_KETA >( v1 ) += v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator -( const integer< MAX_KETA > &v1, const integer< MAX_KETA > &v2 ){ return( integer< MAX_KETA >( v1 ) -= v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator *( const integer< MAX_KETA > &v1, const integer< MAX_KETA > &v2 ){ return( integer< MAX_KETA >( v1 ) *= v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator /( const integer< MAX_KETA > &v1, const integer< MAX_KETA > &v2 ){ return( integer< MAX_KETA >( v1 ) /= v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator %( const integer< MAX_KETA > &v1, const integer< MAX_KETA > &v2 ){ return( integer< MAX_KETA >( v1 ) %= v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator |( const integer< MAX_KETA > &v1, const integer< MAX_KETA > &v2 ){ return( integer< MAX_KETA >( v1 ) |= v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator &( const integer< MAX_KETA > &v1, const integer< MAX_KETA > &v2 ){ return( integer< MAX_KETA >( v1 ) &= v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator ^( const integer< MAX_KETA > &v1, const integer< MAX_KETA > &v2 ){ return( integer< MAX_KETA >( v1 ) ^= v2 ); }
 
-template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator *( const integer< MAX_KETA, T, BASE > &v1, const typename integer< MAX_KETA, T, BASE >::value_type &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) *= v2 ); }
-template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator *( const typename integer< MAX_KETA, T, BASE >::value_type &v1, const integer< MAX_KETA, T, BASE > &v2 ){ return( integer< MAX_KETA, T, BASE >( v2 ) *= v1 ); }
-template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator /( const integer< MAX_KETA, T, BASE > &v1, const typename integer< MAX_KETA, T, BASE >::value_type &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) /= v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator *( const integer< MAX_KETA > &v1, const typename integer< MAX_KETA >::difference_type &v2 ){ return( integer< MAX_KETA >( v1 ) *= v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator *( const typename integer< MAX_KETA >::difference_type &v1, const integer< MAX_KETA > &v2 ){ return( integer< MAX_KETA >( v2 ) *= v1 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator /( const integer< MAX_KETA > &v1, const typename integer< MAX_KETA >::difference_type &v2 ){ return( integer< MAX_KETA >( v1 ) /= v2 ); }
 
-template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator +( const integer< MAX_KETA, T, BASE > &v1, const typename integer< MAX_KETA, T, BASE >::value_type &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) += v2 ); }
-template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator +( const typename integer< MAX_KETA, T, BASE >::value_type &v1, const integer< MAX_KETA, T, BASE > &v2 ){ return( integer< MAX_KETA, T, BASE >( v2 ) += v1 ); }
-template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator -( const integer< MAX_KETA, T, BASE > &v1, const typename integer< MAX_KETA, T, BASE >::value_type &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) -= v2 ); }
-template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator -( const typename integer< MAX_KETA, T, BASE >::value_type &v1, const integer< MAX_KETA, T, BASE > &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) -= v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator +( const integer< MAX_KETA > &v1, const typename integer< MAX_KETA >::difference_type &v2 ){ return( integer< MAX_KETA >( v1 ) += v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator +( const typename integer< MAX_KETA >::difference_type &v1, const integer< MAX_KETA > &v2 ){ return( integer< MAX_KETA >( v2 ) += v1 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator -( const integer< MAX_KETA > &v1, const typename integer< MAX_KETA >::difference_type &v2 ){ return( integer< MAX_KETA >( v1 ) -= v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator -( const typename integer< MAX_KETA >::difference_type &v1, const integer< MAX_KETA > &v2 ){ return( integer< MAX_KETA >( v1 ) -= v2 ); }
 
-template < unsigned int MAX_KETA, class T, unsigned int BASE > inline const integer< MAX_KETA, T, BASE > operator %( const integer< MAX_KETA, T, BASE > &v1, const typename integer< MAX_KETA, T, BASE >::value_type &v2 ){ return( integer< MAX_KETA, T, BASE >( v1 ) %= v2 ); }
+template < unsigned int MAX_KETA > inline const integer< MAX_KETA > operator %( const integer< MAX_KETA > &v1, const typename integer< MAX_KETA >::difference_type &v2 ){ return( integer< MAX_KETA >( v1 ) %= v2 ); }
 
 
-template < unsigned int MAX_KETA, class T, unsigned int BASE >
-inline std::ostream &operator <<( std::ostream &out, const integer< MAX_KETA, T, BASE > &a )
+template < unsigned int MAX_KETA >
+inline std::ostream &operator <<( std::ostream &out, const integer< MAX_KETA > &a )
 {
 	out << a.to_string( );
 	return( out );
--- test/integer_test.cpp
+++ test/integer_test.cpp
@@ -6,7 +6,7 @@ int main( int argc, char *argv[] )
 	using namespace std;
 
 //	mist::integer< 21 > v1( 100000 ), v2( 2 );
-	mist::integer< 10, char, 10 > v1( 900 ), v2( 1000 );
+	mist::integer< 20 > v1( 900 ), v2( "1234567890123456789" );
 //	mist::integer< 10, short, 100 > v1( 900 ), v2( 1000 );
 
 
@@ -24,9 +24,9 @@ int main( int argc, char *argv[] )
 	cout << "11. Operator A / 2 = \t"; cout << ( v1 / 2  ) << endl;
 
 	cout << "12. Operator A % B = \t"; cout << ( v1 % v2 ) << endl;
-	//cout << "13. Operator A | B = \t"; cout << ( v1 | v2 ) << endl;
-	//cout << "14. Operator A & B = \t"; cout << ( v1 & v2 ) << endl;
-	//cout << "15. Operator A ^ B = \t"; cout << ( v1 ^ v2 ) << endl;
+	cout << "13. Operator A | B = \t"; cout << ( v1 | v2 ) << endl;
+	cout << "14. Operator A & B = \t"; cout << ( v1 & v2 ) << endl;
+	cout << "15. Operator A ^ B = \t"; cout << ( v1 ^ v2 ) << endl;
 
 	cout << "16. Operator A == B = \t"; cout << ( v1 == v2 ) << endl;
 	cout << "17. Operator A != B = \t"; cout << ( v1 != v2 ) << endl;
