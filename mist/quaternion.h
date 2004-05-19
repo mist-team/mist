33e026bdbfcb5744670b6ef234f9fc087a184ac1
Daisuke Deguchi <ddeguchi@nagoya-u.jp>
2004-05-19 05:49:49 +0000

    トラックボールをためしに追加

--- mist/quaternion.h
+++ mist/quaternion.h
@@ -219,6 +219,81 @@ template < class T > inline std::ostream &operator <<( std::ostream &out, const 
 //	return( mat );
 //}
 
+// 仮想トラックボールの実装(左手座標系)
+// 以下のソースコードを参考にした
+//
+//* Trackball code:
+//*
+//* Implementation of a virtual trackball.
+//* Implemented by Gavin Bell, lots of ideas from Thant Tessman and
+//*   the August '88 issue of Siggraph's "Computer Graphics," pp. 121-129.
+//*
+//* Vector manip code:
+//*
+//* Original code from:
+//* David M. Ciemiewicz, Mark Grossman, Henry Moreton, and Paul Haeberli
+//*
+//* Much mucking with by:
+//* Gavin Bell
+//
+template < class T >
+const quaternion< T > track_ball( const vector2< T > &p1, const vector2< T > &p2, const vector3< T > &axisX, const vector3< T > axisY, const vector3< T > axisZ, const typename vector3< T >::value_type &trackball_size = 0.8 )
+{
+	typedef typename quaternion< T >::value_type value_type;
+
+	if( p1 == p2 )
+	{
+		return( quaternion< T >( 1, 0, 0, 0 ) );
+	}
+
+	vector3< T > sp1( p1.x, p1.y, 0 ), sp2( p2.x, p2.y, 0 );
+	value_type l, _2 = std::sqrt( value_type( 2.0 ) );
+
+	// 点1の座標を仮想トラックボール上に投影
+	l = p1.length( );
+	if( l < trackball_size / _2 )
+	{
+		sp1.z = - std::sqrt( trackball_size * trackball_size - l * l );
+	}
+	else
+	{
+		sp1.z = - trackball_size * trackball_size / 2.0 / l;
+	}
+
+	// 点2の座標を仮想トラックボール上に投影
+	l = p2.length( );
+	if( l < trackball_size / _2 )
+	{
+		sp2.z = - std::sqrt( trackball_size * trackball_size - l * l );
+	}
+	else
+	{
+		sp2.z = - trackball_size * trackball_size / 2.0 / l;
+	}
+
+	//	sp1 = sp1.unit();
+	//	sp2 = sp2.unit();
+
+	//	Vector3<double> axis = (sp2 * sp1).unit();
+	vector3< T > axis = ( sp1 * sp2 ).unit( );
+	axis = ( axis.x * axisX + axis.y * axisY + axis.z * axisZ ).unit( );
+
+	l = ( sp2 - sp1 ).length( ) / ( 2 * trackball_size );
+	//	l = (l < -1.0)? -1.0: l;
+	l = l > 1 ? 1: l;
+
+	double phi = std::asin( l );
+	//	fprintf(stdout, "axis(%.1f, %.1f, %.1f)   theta = %.1f\n", axis.x, axis.y, axis.z, phi * 180 / PAI);
+	//	printf("%.1f\n", phi * 180 / PAI);
+	return( quaternion< T >( std::cos( phi ), std::sin( phi ) * axis ) );
+}
+
+template < class T >
+const quaternion< T > track_ball( const typename vector3< T >::value_type &x1, const typename vector3< T >::value_type &y1, const typename vector3< T >::value_type &x2, const typename vector3< T >::value_type &y2,
+									const vector3< T > &axisX, const vector3< T > &axisY, const vector3< T > &axisZ, const typename vector3< T >::value_type &trackball_size = typename vector3< T >::value_type( 0.8 ) )
+{
+	return( track_ball( vector2< T >( x1, y1 ), vector2< T >( x2, y2 ), axisX, axisY, axisZ, trackball_size ) );
+}
 
 
 // mist名前空間の終わり
