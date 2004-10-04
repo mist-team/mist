3f8ed4c1915858ad281c13b75a20144769117469
Daisuke Deguchi <ddeguchi@nagoya-u.jp>
2004-10-04 14:12:59 +0000

    これまでは，ラベリング中に初期に指定した最大ラベル数を超えると正しくラベリングが行われない使用だった． そこで，ラベル数が足りなくなったときに自動的に拡張するように変更を行った． それに伴い，ラベル数が増え続けるとメモリ使用量が増大する可能性があります．

--- mist/filter/labeling.h
+++ mist/filter/labeling.h
@@ -10,6 +10,7 @@
 #ifndef __INCLUDE_MIST_LABELING__
 #define __INCLUDE_MIST_LABELING__
 
+#include <vector>
 
 #ifndef __INCLUDE_MIST_H__
 #include "../mist.h"
@@ -67,8 +68,8 @@ namespace __labeling_controller__
 	{
 		_MIST_CONST( size_t, array_num, 13 );
 
-		template < class Array >
-		static inline typename Array::size_type neighbor( Array &in, const typename Array::size_type *T, typename Array::size_type *L,
+		template < class Array, class Vector >
+		static inline typename Array::size_type neighbor( Array &in, const Vector &T, typename Array::size_type *L,
 										const typename Array::size_type i, const typename Array::size_type j, const typename Array::size_type k,
 										const typename Array::size_type w, const typename Array::size_type h, const typename Array::size_type d )
 		{
@@ -97,8 +98,8 @@ namespace __labeling_controller__
 	{
 		_MIST_CONST( size_t, array_num, 9 );
 
-		template < class Array >
-		static inline typename Array::size_type neighbor( Array &in, const typename Array::size_type *T, typename Array::size_type *L,
+		template < class Array, class Vector >
+		static inline typename Array::size_type neighbor( Array &in, const Vector &T, typename Array::size_type *L,
 										const typename Array::size_type i, const typename Array::size_type j, const typename Array::size_type k,
 										const typename Array::size_type w, const typename Array::size_type h, const typename Array::size_type d )
 		{
@@ -123,8 +124,8 @@ namespace __labeling_controller__
 	{
 		_MIST_CONST( size_t, array_num, 3 );
 
-		template < class Array >
-		static inline typename Array::size_type neighbor( Array &in, const typename Array::size_type *T, typename Array::size_type *L,
+		template < class Array, class Vector >
+		static inline typename Array::size_type neighbor( Array &in, const Vector &T, typename Array::size_type *L,
 										const typename Array::size_type i, const typename Array::size_type j, const typename Array::size_type k,
 										const typename Array::size_type w, const typename Array::size_type h, const typename Array::size_type d )
 		{
@@ -143,8 +144,8 @@ namespace __labeling_controller__
 	{
 		_MIST_CONST( size_t, array_num, 4 );
 
-		template < class Array >
-		static inline typename Array::size_type neighbor( Array &in, const typename Array::size_type *T, typename Array::size_type *L,
+		template < class Array, class Vector >
+		static inline typename Array::size_type neighbor( Array &in, const Vector &T, typename Array::size_type *L,
 										const typename Array::size_type i, const typename Array::size_type j, const typename Array::size_type k,
 										const typename Array::size_type w, const typename Array::size_type h, const typename Array::size_type d )
 		{
@@ -164,8 +165,8 @@ namespace __labeling_controller__
 	{
 		_MIST_CONST( size_t, array_num, 2 );
 
-		template < class Array >
-		static inline typename Array::size_type neighbor( Array &in, const typename Array::size_type *T, typename Array::size_type *L,
+		template < class Array, class Vector >
+		static inline typename Array::size_type neighbor( Array &in, const Vector &T, typename Array::size_type *L,
 										const typename Array::size_type i, const typename Array::size_type j, const typename Array::size_type k,
 										const typename Array::size_type w, const typename Array::size_type h, const typename Array::size_type d )
 		{
@@ -180,7 +181,7 @@ namespace __labeling_controller__
 
 
 	template < class Array, class neighbor, class Functor >
-	typename Array::size_type labeling( Array &in, const typename Array::size_type label_max, const neighbor dmy, Functor f )
+	typename Array::size_type labeling( Array &in, typename Array::size_type label_max, const neighbor dmy, Functor f )
 	{
 		typedef typename Array::size_type  size_type;
 		typedef typename Array::value_type value_type;
@@ -188,17 +189,19 @@ namespace __labeling_controller__
 		size_type label_num = 0;
 		size_type i, j, k, l, count;
 
-		size_type *T = new size_type[ label_max * 4 + 1 ];
+		std::vector< size_type > T;
 		size_type L[ neighbor::array_num ];
 		const size_type width = in.width( );
 		const size_type height = in.height( );
 		const size_type depth = in.depth( );
 
-		T[ 0 ] = T[ 1 ] = 0;
-
+		T.reserve( label_max );
+		T.push_back( 0 );	// T[ 0 ]
 
 		const bool bprogress1 = depth == 1;
 		const bool bprogress2 = depth >  1;
+
+		label_max = type_limits< value_type >::maximum( );
 
 		f( 0.0 );
 
@@ -218,7 +221,7 @@ namespace __labeling_controller__
 					neighbor::neighbor( in, T, L, i, j, k, width, height, depth );
 
 					// 近傍で最小のラベル番号を持つものを取得し，ラベル番号が0で無い物の数を数え上げる
-					size_type L1 = type_limits< value_type >::maximum( );
+					size_type L1 = label_max;
 					for( l = 0, count = 0 ; l < neighbor::array_num ; l++ )
 					{
 						if( L[ l ] > 0 )
@@ -237,11 +240,13 @@ namespace __labeling_controller__
 					if( count == 0 )
 					{
 						// 近傍に，すでに割り当てられているラベルは存在しないため，新規にラベルを割り当てる
+						// 出力ラベル値が出力データ型の最大値を超えなければ，ラベル数を更新する
 						if( label_num < label_max )
 						{
 							label_num++;
 						}
-						T[ label_num ] = label_num;
+						T.push_back( label_num );
+
 						in( i, j, k ) = static_cast< value_type >( label_num );
 					}
 					else if( count == 1 )
@@ -324,7 +329,6 @@ namespace __labeling_controller__
 
 		f( 100.1 );
 
-		delete [] T;
 		delete [] NT;
 
 		return( label_num );
@@ -344,8 +348,11 @@ namespace __labeling_controller__
 /// @brief 2次元画像に対する4近傍型ラベリング
 //! 
 //! 2次元画像に対する4近傍型ラベリング
+//! 
 //! @attention 入力と出力が同じ画像オブジェクトでも正しくラベリングすることが可能です
 //! @attention ただし，データ型が char 型を利用する場合は，ラベル数がオーバーフローしないように注意が必要
+//! @attention また，最大ラベル数を超えた場合は自動的に最大ラベル数を更新する．そのため，メモリ使用量が増加する可能性がある．
+//! @attention 初期値の max_label を大きくとることでメモリの再確保が減り高速に動作するようになる（そのかわり，初期メモリ量が増加する）．
 //! 
 //! @param[in]  in        … 入力画像
 //! @param[out] out       … 出力画像
@@ -380,8 +387,11 @@ typename array2< T2, Allocator2 >::size_type labeling4( const array2< T1, Alloca
 /// @brief 2次元画像に対する4近傍型ラベリング
 //! 
 //! 2次元画像に対する4近傍型ラベリング
+//! 
 //! @attention 入力と出力が同じ画像オブジェクトでも正しくラベリングすることが可能です
 //! @attention ただし，データ型が char 型を利用する場合は，ラベル数がオーバーフローしないように注意が必要
+//! @attention また，最大ラベル数を超えた場合は自動的に最大ラベル数を更新する．そのため，メモリ使用量が増加する可能性がある．
+//! @attention 初期値の max_label を大きくとることでメモリの再確保が減り高速に動作するようになる（そのかわり，初期メモリ量が増加する）．
 //! 
 //! @param[in]  in        … 入力画像
 //! @param[out] out       … 出力画像
@@ -399,8 +409,11 @@ typename array2< T2, Allocator2 >::size_type labeling4( const array2< T1, Alloca
 /// @brief 2次元画像に対する8近傍型ラベリング
 //! 
 //! 2次元画像に対する8近傍型ラベリング
+//! 
 //! @attention 入力と出力が同じ画像オブジェクトでも正しくラベリングすることが可能です
 //! @attention ただし，データ型が char 型を利用する場合は，ラベル数がオーバーフローしないように注意が必要
+//! @attention また，最大ラベル数を超えた場合は自動的に最大ラベル数を更新する．そのため，メモリ使用量が増加する可能性がある．
+//! @attention 初期値の max_label を大きくとることでメモリの再確保が減り高速に動作するようになる（そのかわり，初期メモリ量が増加する）．
 //! 
 //! @param[in]  in        … 入力画像
 //! @param[out] out       … 出力画像
@@ -435,8 +448,11 @@ typename array2< T2, Allocator2 >::size_type labeling8( const array2< T1, Alloca
 /// @brief 2次元画像に対する8近傍型ラベリング
 //! 
 //! 2次元画像に対する8近傍型ラベリング
+//! 
 //! @attention 入力と出力が同じ画像オブジェクトでも正しくラベリングすることが可能です
 //! @attention ただし，データ型が char 型を利用する場合は，ラベル数がオーバーフローしないように注意が必要
+//! @attention また，最大ラベル数を超えた場合は自動的に最大ラベル数を更新する．そのため，メモリ使用量が増加する可能性がある．
+//! @attention 初期値の max_label を大きくとることでメモリの再確保が減り高速に動作するようになる（そのかわり，初期メモリ量が増加する）．
 //! 
 //! @param[in]  in        … 入力画像
 //! @param[out] out       … 出力画像
@@ -453,9 +469,12 @@ inline typename array2< T2, Allocator2 >::size_type labeling8( const array2< T1,
 
 /// @brief 3次元画像に対する6近傍型ラベリング
 //! 
-//! 2次元画像に対する6近傍型ラベリング
+//! 3次元画像に対する6近傍型ラベリング
+//! 
 //! @attention 入力と出力が同じ画像オブジェクトでも正しくラベリングすることが可能です
 //! @attention ただし，データ型が char 型を利用する場合は，ラベル数がオーバーフローしないように注意が必要
+//! @attention また，最大ラベル数を超えた場合は自動的に最大ラベル数を更新する．そのため，メモリ使用量が増加する可能性がある．
+//! @attention 初期値の max_label を大きくとることでメモリの再確保が減り高速に動作するようになる（そのかわり，初期メモリ量が増加する）．
 //! 
 //! @param[in]  in        … 入力画像
 //! @param[out] out       … 出力画像
@@ -489,9 +508,12 @@ typename array3< T2, Allocator2 >::size_type labeling6( const array3< T1, Alloca
 
 /// @brief 3次元画像に対する6近傍型ラベリング
 //! 
-//! 2次元画像に対する6近傍型ラベリング
+//! 3次元画像に対する6近傍型ラベリング
+//! 
 //! @attention 入力と出力が同じ画像オブジェクトでも正しくラベリングすることが可能です
 //! @attention ただし，データ型が char 型を利用する場合は，ラベル数がオーバーフローしないように注意が必要
+//! @attention また，最大ラベル数を超えた場合は自動的に最大ラベル数を更新する．そのため，メモリ使用量が増加する可能性がある．
+//! @attention 初期値の max_label を大きくとることでメモリの再確保が減り高速に動作するようになる（そのかわり，初期メモリ量が増加する）．
 //! 
 //! @param[in]  in        … 入力画像
 //! @param[out] out       … 出力画像
@@ -508,13 +530,16 @@ inline typename array3< T2, Allocator2 >::size_type labeling6( const array3< T1,
 
 /// @brief 3次元画像に対する18近傍型ラベリング
 //! 
-//! 2次元画像に対する18近傍型ラベリング
+//! 3次元画像に対する18近傍型ラベリング
+//! 
 //! @attention 入力と出力が同じ画像オブジェクトでも正しくラベリングすることが可能です
 //! @attention ただし，データ型が char 型を利用する場合は，ラベル数がオーバーフローしないように注意が必要
+//! @attention また，最大ラベル数を超えた場合は自動的に最大ラベル数を更新する．そのため，メモリ使用量が増加する可能性がある．
+//! @attention 初期値の max_label を大きくとることでメモリの再確保が減り高速に動作するようになる（そのかわり，初期メモリ量が増加する）．
 //! 
 //! @param[in]  in        … 入力画像
 //! @param[out] out       … 出力画像
-//! @param[in]  max_label … 最大で割り当てるラベル数
+//! @param[in]  max_label … 最大で割り当てるラベル数のデフォルト値
 //! @param[in]  f         … 進行状況を返すコールバック関数
 //! 
 //! @return 割り当てられたラベル数
@@ -544,9 +569,12 @@ typename array3< T2, Allocator2 >::size_type labeling18( const array3< T1, Alloc
 
 /// @brief 3次元画像に対する18近傍型ラベリング
 //! 
-//! 2次元画像に対する18近傍型ラベリング
+//! 3次元画像に対する18近傍型ラベリング
+//! 
 //! @attention 入力と出力が同じ画像オブジェクトでも正しくラベリングすることが可能です
 //! @attention ただし，データ型が char 型を利用する場合は，ラベル数がオーバーフローしないように注意が必要
+//! @attention また，最大ラベル数を超えた場合は自動的に最大ラベル数を更新する．そのため，メモリ使用量が増加する可能性がある．
+//! @attention 初期値の max_label を大きくとることでメモリの再確保が減り高速に動作するようになる（そのかわり，初期メモリ量が増加する）．
 //! 
 //! @param[in]  in        … 入力画像
 //! @param[out] out       … 出力画像
@@ -563,9 +591,12 @@ inline typename array3< T2, Allocator2 >::size_type labeling18( const array3< T1
 
 /// @brief 3次元画像に対する26近傍型ラベリング
 //! 
-//! 2次元画像に対する26近傍型ラベリング
+//! 3次元画像に対する26近傍型ラベリング
+//! 
 //! @attention 入力と出力が同じ画像オブジェクトでも正しくラベリングすることが可能です
 //! @attention ただし，データ型が char 型を利用する場合は，ラベル数がオーバーフローしないように注意が必要
+//! @attention また，最大ラベル数を超えた場合は自動的に最大ラベル数を更新する．そのため，メモリ使用量が増加する可能性がある．
+//! @attention 初期値の max_label を大きくとることでメモリの再確保が減り高速に動作するようになる（そのかわり，初期メモリ量が増加する）．
 //! 
 //! @param[in]  in        … 入力画像
 //! @param[out] out       … 出力画像
@@ -600,9 +631,12 @@ typename array3< T2, Allocator2 >::size_type labeling26( const array3< T1, Alloc
 
 /// @brief 3次元画像に対する26近傍型ラベリング
 //! 
-//! 2次元画像に対する26近傍型ラベリング
+//! 3次元画像に対する26近傍型ラベリング
+//! 
 //! @attention 入力と出力が同じ画像オブジェクトでも正しくラベリングすることが可能です
 //! @attention ただし，データ型が char 型を利用する場合は，ラベル数がオーバーフローしないように注意が必要
+//! @attention また，最大ラベル数を超えた場合は自動的に最大ラベル数を更新する．そのため，メモリ使用量が増加する可能性がある．
+//! @attention 初期値の max_label を大きくとることでメモリの再確保が減り高速に動作するようになる（そのかわり，初期メモリ量が増加する）．
 //! 
 //! @param[in]  in        … 入力画像
 //! @param[out] out       … 出力画像
@@ -615,6 +649,216 @@ inline typename array3< T2, Allocator2 >::size_type labeling26( const array3< T1
 {
 	return( labeling26( in, out, max_label, __mist_dmy_callback__( ) ) );
 }
+
+
+
+
+/// @brief ある範囲内での最大のラベルを抽出する
+//! 
+//! 3次元画像に対する8近傍型ラベリングを用いて，最大連結成分を抽出する
+//! 
+//! @attention 入力と出力が同じ画像オブジェクトでも正しく動作する
+//! 
+//! @param[in]  in        … 入力画像
+//! @param[out] out       … 出力画像
+//! @param[in]  sx        … X軸方向の小さい側の位置
+//! @param[in]  ex        … X軸方向の大きい側の位置
+//! @param[in]  sy        … Y軸方向の小さい側の位置
+//! @param[in]  ey        … Y軸方向の大きい側の位置
+//! @param[in]  max_label … 最大で割り当てるラベル数
+//! 
+//! @return 割り当てられたラベル数
+//! 
+template < class T1,  class T2, class Allocator1, class Allocator2 >
+void maximum_region(
+						const array2< T1, Allocator1 > &in,
+						array2< T2, Allocator2 > &out,
+						typename array2< T1, Allocator1 >::size_type sx,
+						typename array2< T1, Allocator1 >::size_type ex,
+						typename array2< T1, Allocator1 >::size_type sy,
+						typename array2< T1, Allocator1 >::size_type ey,
+						typename array2< T1, Allocator1 >::size_type max_label = __labeling_controller__::default_label_num2< T2 >::value
+					)
+{
+	typedef typename array2< T1, Allocator1 >::size_type size_type;
+	typedef typename array2< T1, Allocator1 >::difference_type difference_type;
+
+	array2< int > tmp;
+	size_type i, j, k;
+
+	size_type label_num = mist::labeling8( in, tmp, max_label );
+
+	// 指定された範囲内の最大ラベルを探索
+	sx = sx < 0 ? 0 : sx;
+	sy = sy < 0 ? 0 : sy;
+	sx = sx < in.width( )  ? sx : in.width( )  - 1;
+	sy = sy < in.height( ) ? sy : in.height( ) - 1;
+
+	ex = ex < 0 ? 0 : ex;
+	ey = ey < 0 ? 0 : ey;
+	ex = ex < in.width( )  ? ex : in.width( )  - 1;
+	ey = ey < in.height( ) ? ey : in.height( ) - 1;
+
+	size_type *menseki = new size_type[ label_num + 1 ];
+	for( i = 0 ; i <= label_num ; i++ )
+	{
+		menseki[ i ] = 0;
+	}
+
+	for( j = sy ; j <= ey ; j++ )
+	{
+		for( i = sx ; i <= ex ; i++ )
+		{
+			menseki[ tmp( i, j, k ) ]++;
+		}
+	}
+
+	size_type max_label = 1;
+	for( i = 2 ; i <= label_num ; i++ )
+	{
+		max_label = menseki[ i ] > menseki[ max_label ] ? i : max_label;
+	}
+	delete [] menseki;
+
+	out.resize( in.size1( ), in.size2( ) );
+	out.reso1( in.reso1( ) );
+	out.reso2( in.reso2( ) );
+
+	for( i = 0 ; i < out.size( ) ; i++ )
+	{
+		out[ i ] = tmp[ i ] == max_label ? 1 : 0;
+	}
+}
+
+
+/// @brief 画像の全範囲内での最大のラベルを抽出する
+//! 
+//! 3次元画像に対する8近傍型ラベリングを用いて，最大連結成分を抽出する
+//! 
+//! @attention 入力と出力が同じ画像オブジェクトでも正しく動作する
+//! 
+//! @param[in]  in        … 入力画像
+//! @param[out] out       … 出力画像
+//! @param[in]  max_label … 最大で割り当てるラベル数
+//! 
+//! @return 割り当てられたラベル数
+//! 
+template < class T1,  class T2, class Allocator1, class Allocator2 >
+void maximum_region( const array2< T1, Allocator1 > &in, array2< T2, Allocator2 > &out, typename array2< T1, Allocator1 >::size_type max_label = __labeling_controller__::default_label_num2< T2 >::value )
+{
+	maximum_region( in, out, 0, in.width( ) - 1, 0, in.height( ) - 1, max_label );
+}
+
+
+
+/// @brief ある範囲内での最大のラベルを抽出する
+//! 
+//! 3次元画像に対する26近傍型ラベリングを用いて，最大連結成分を抽出する
+//! 
+//! @attention 入力と出力が同じ画像オブジェクトでも正しく動作する
+//! 
+//! @param[in]  in        … 入力画像
+//! @param[out] out       … 出力画像
+//! @param[in]  sx        … X軸方向の小さい側の位置
+//! @param[in]  ex        … X軸方向の大きい側の位置
+//! @param[in]  sy        … Y軸方向の小さい側の位置
+//! @param[in]  ey        … Y軸方向の大きい側の位置
+//! @param[in]  sz        … Z軸方向の小さい側の位置
+//! @param[in]  ez        … Z軸方向の大きい側の位置
+//! @param[in]  max_label … 最大で割り当てるラベル数
+//! 
+//! @return 割り当てられたラベル数
+//! 
+template < class T1,  class T2, class Allocator1, class Allocator2 >
+void maximum_region(
+						const array3< T1, Allocator1 > &in,
+						array3< T2, Allocator2 > &out,
+						typename array3< T1, Allocator1 >::size_type sx,
+						typename array3< T1, Allocator1 >::size_type ex,
+						typename array3< T1, Allocator1 >::size_type sy,
+						typename array3< T1, Allocator1 >::size_type ey,
+						typename array3< T1, Allocator1 >::size_type sz,
+						typename array3< T1, Allocator1 >::size_type ez,
+						typename array3< T1, Allocator1 >::size_type max_label = __labeling_controller__::default_label_num3< T2 >::value
+					)
+{
+	typedef typename array3< T1, Allocator1 >::size_type size_type;
+	typedef typename array3< T1, Allocator1 >::difference_type difference_type;
+
+	array3< int > tmp;
+	size_type i, j, k;
+
+	size_type label_num = mist::labeling26( in, tmp, max_label );
+
+	// 指定された範囲内の最大ラベルを探索
+	sx = sx < 0 ? 0 : sx;
+	sy = sy < 0 ? 0 : sy;
+	sz = sz < 0 ? 0 : sz;
+	sx = sx < in.width( )  ? sx : in.width( )  - 1;
+	sy = sy < in.height( ) ? sy : in.height( ) - 1;
+	sz = sz < in.depth( )  ? sz : in.depth( )  - 1;
+
+	ex = ex < 0 ? 0 : ex;
+	ey = ey < 0 ? 0 : ey;
+	ez = ez < 0 ? 0 : ez;
+	ex = ex < in.width( )  ? ex : in.width( )  - 1;
+	ey = ey < in.height( ) ? ey : in.height( ) - 1;
+	ez = ez < in.depth( )  ? ez : in.depth( )  - 1;
+
+	size_type *menseki = new size_type[ label_num + 1 ];
+	for( i = 0 ; i <= label_num ; i++ )
+	{
+		menseki[ i ] = 0;
+	}
+
+	for( k = sz ; k <= ez ; k++ )
+	{
+		for( j = sy ; j <= ey ; j++ )
+		{
+			for( i = sx ; i <= ex ; i++ )
+			{
+				menseki[ tmp( i, j, k ) ]++;
+			}
+		}
+	}
+
+	size_type max_label = 1;
+	for( i = 2 ; i <= label_num ; i++ )
+	{
+		max_label = menseki[ i ] > menseki[ max_label ] ? i : max_label;
+	}
+	delete [] menseki;
+
+	out.resize( in.size1( ), in.size2( ), in.size3( ) );
+	out.reso1( in.reso1( ) );
+	out.reso2( in.reso2( ) );
+	out.reso3( in.reso3( ) );
+
+	for( i = 0 ; i < out.size( ) ; i++ )
+	{
+		out[ i ] = tmp[ i ] == max_label ? 1 : 0;
+	}
+}
+
+
+/// @brief 画像の全範囲内での最大のラベルを抽出する
+//! 
+//! 3次元画像に対する6近傍型ラベリングを用いて，最大連結成分を抽出する
+//! 
+//! @attention 入力と出力が同じ画像オブジェクトでも正しく動作する
+//! 
+//! @param[in]  in        … 入力画像
+//! @param[out] out       … 出力画像
+//! @param[in]  max_label … 最大で割り当てるラベル数
+//! 
+//! @return 割り当てられたラベル数
+//! 
+template < class T1,  class T2, class Allocator1, class Allocator2 >
+void maximum_region( const array3< T1, Allocator1 > &in, array3< T2, Allocator2 > &out, typename array3< T1, Allocator1 >::size_type max_label = __labeling_controller__::default_label_num3< T2 >::value )
+{
+	maximum_region( in, out, 0, in.width( ) - 1, 0, in.height( ) - 1, 0, in.depth( ) - 1, max_label );
+}
+
 
 /// @}
 //  ラベリンググループの終わり
