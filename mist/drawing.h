3bcd2b01adb08511b6d230ee7e6bbd0942cf03f8
Daisuke Deguchi <ddeguchi@nagoya-u.jp>
2006-05-12 06:37:20 +0000

    ビットマップ画像に対する処理を追加

--- mist/drawing.h
+++ mist/drawing.h
@@ -38,6 +38,10 @@
 #include "mist.h"
 #endif
 
+#ifndef __INCLUDE_BITMAP_H__
+#include "bitmap.h"
+#endif
+
 #include <cmath>
 
 
@@ -77,6 +81,79 @@ void draw_line( array2< T, Allocator > &image,
 			   typename array2< T, Allocator >::value_type color = typename array2< T, Allocator >::value_type( ) )
 {
 	typedef typename array2< T, Allocator >::difference_type difference_type;
+	difference_type dx, dy;
+	difference_type sx, sy;
+	difference_type x, y, e, i;
+	difference_type w = static_cast< difference_type >( image.width( ) );
+	difference_type h = static_cast< difference_type >( image.height( ) );
+
+	dx = std::abs( static_cast< int >( x2 - x1 ) );
+	dy = std::abs( static_cast< int >( y2 - y1 ) );
+	sx = ( x2 - x1 >=0 ) ? 1 : -1;
+	sy = ( y2 - y1 >=0 ) ? 1 : -1;
+	x = x1;
+	y = y1;
+	if( dx >= dy )
+	{
+		e = -dx;
+		for( i = 0 ; i <= dx ; i++ )
+		{
+			if( x >= 0 && x < w && y >= 0 && y < h )
+			{
+				image( x, y ) = color;
+			}
+			x += sx;
+			e += 2 * dy;
+
+			if( e >= 0 )
+			{
+				y += sy;
+				e -= 2 * dx;
+			}
+		}
+	}
+	else
+	{
+		e = -dy;
+		for( i = 0 ; i <= dy ; i++ )
+		{
+			if( x >= 0 && x < w && y >= 0 && y < h )
+			{
+				image( x, y ) = color;
+			}
+			y += sy;
+			e += 2 * dx;
+			if( e >= 0 )
+			{
+				x += sx;
+				e -= 2 * dy;
+			}
+		}
+	}
+}
+
+
+
+/// @brief 2次元ビットマップ画像に直線を描画する
+//! 
+//! 手法について何か書く
+//! 
+//! @param[in,out] image … 入出力画像
+//! @param[in]     x1    … 直線の端点1のX座標
+//! @param[in]     y1    … 直線の端点1のY座標
+//! @param[in]     x2    … 直線の端点2のX座標
+//! @param[in]     y2    … 直線の端点2のY座標
+//! @param[in]     color … 直線の色もしくは値
+//!
+template < size_t BITS >
+void draw_line( bitmap< BITS > &image,
+			   typename bitmap< BITS >::difference_type x1,
+			   typename bitmap< BITS >::difference_type y1,
+			   typename bitmap< BITS >::difference_type x2,
+			   typename bitmap< BITS >::difference_type y2,
+			   typename bitmap< BITS >::value_type color = typename bitmap< BITS >::value_type( ) )
+{
+	typedef typename bitmap< BITS >::difference_type difference_type;
 	difference_type dx, dy;
 	difference_type sx, sy;
 	difference_type x, y, e, i;
