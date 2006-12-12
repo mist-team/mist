118683f2633efc934210b98e80ca970b635dc8a2
Yukitaka Nimura <ynimura@mori.m.is.nagoya-u.ac.jp>
2006-12-12 04:14:40 +0000

    テストを失敗した箇所のみ，その詳細を表示するように変更した．

--- unittest/main.cpp
+++ unittest/main.cpp
@@ -3,10 +3,10 @@
 #include <cppunit/TestRunner.h>
 #include <cppunit/TestResult.h>
 #include <cppunit/TestResultCollector.h>
+//#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/TextOutputter.h>
 //#include <cppunit/extensions/HelperMacros.h>
-#include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
-
 
 int main( int argc, char **argv )
 {
@@ -18,13 +18,17 @@ int main( int argc, char **argv )
 	controller.addListener( &result );        
 
 	// テスト実行時に進行状況を表示するオブジェクトを作成する
-	CPPUNIT_NS::BriefTestProgressListener progress;
-	controller.addListener( &progress );      
+	//CPPUNIT_NS::BriefTestProgressListener progress;
+	//controller.addListener( &progress );
 
 	// テストランナーにテストを設定する
 	CPPUNIT_NS::TestRunner runner;
 	runner.addTest( CPPUNIT_NS::TestFactoryRegistry::getRegistry( ).makeTest( ) );
 	runner.run( controller );
+
+	// テスト結果を出力する
+	CPPUNIT_NS::TextOutputter *outputter = new CPPUNIT_NS::TextOutputter( &result, std::cout );
+	outputter->write( );
 
 	// テストに成功した場合に 0 を返し，失敗したら 1 を返す
 	return( result.wasSuccessful( ) ? 0 : 1 );
