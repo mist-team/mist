904bf87bf335460b2319f3277c1269f21009ac84
Daisuke Deguchi <ddeguchi@nagoya-u.jp>
2009-02-05 05:25:45 +0000

    Copyrightを2009年に変更した．

--- test/crc_test.cpp
+++ test/crc_test.cpp
@@ -1,5 +1,5 @@
 // 
-// Copyright (c) 2003-2008, MIST Project, Intelligent Media Integration COE, Nagoya University
+// Copyright (c) 2003-2009, MIST Project, Intelligent Media Integration COE, Nagoya University
 // All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without modification,
