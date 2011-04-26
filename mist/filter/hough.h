31eafacb1206337b9ceb355f91f76c48bea58b35
Daisuke Deguchi <ddeguchi@nagoya-u.jp>
2011-04-26 04:10:24 +0000

    Updated the copyright year to 2011.

--- mist/filter/hough.h
+++ mist/filter/hough.h
@@ -1,5 +1,5 @@
 // 
-// Copyright (c) 2003-2010, Shuichirou Kitou, MIST Project, Nagoya University
+// Copyright (c) 2003-2011, Shuichirou Kitou, MIST Project, Nagoya University
 // All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without modification,
